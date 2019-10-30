95931115edff6e07aec4385b88597b99b734c854
Alexander <vinokurov.ag@mail.ru>
2019-10-30 12:02:58 +0300

    ES6

--- index.js
+++ index.js
@@ -1,73 +1,81 @@
-var Error = function (element) {
-  this.element = element;
-}
-
-Error.prototype.add = function (text) {
-  this.element.textContent = text;
-  this.element.classList.add('error-show');
-  input.style.border = '2px solid red';
-}
-
-Error.prototype.remove = function () {
-  if (this.element.classList.contains('error-show')) {
-    this.element.classList.remove('error-show');
-    this.element.textContent = '';
-    input.style.border = 'none';
+class Error {
+  constructor(element) {
+    this.element = element;
   }
-}
 
-var Animal = function (animalsTypes) {
-  this.animalsTypes = animalsTypes;
-  this.animalsNumber = {};
-  animalsTypes.forEach(function (type) {
-    this.animalsNumber[type] = 0;
-  }.bind(this));
-}
+  add(text) {
+    this.element.textContent = text;
+    this.element.classList.add('error-show');
+    input.style.border = '2px solid red';
+  }
 
-Animal.prototype.getPopularType = function (types) {
-  types = types.split('\n');
-  types.pop();
-  types.forEach(function (type) {
-    this.animalsNumber[type]++
-  }.bind(this));
-
-  var popularType = [];
-  var maxValue = Math.max(...Object.values(this.animalsNumber));
-
-  for (key in this.animalsNumber) {
-    if (this.animalsNumber[key] === maxValue) {
-      popularType.push(key);
+  remove() {
+    if (this.element.classList.contains('error-show')) {
+      this.element.classList.remove('error-show');
+      this.element.textContent = '';
+      input.style.border = 'none';
     }
   }
-
-  return popularType;
 }
 
-Animal.prototype.generateAnimalsTypes = function (numberOfAnimals) {
-
-  for (key in this.animalsNumber) {
-    this.animalsNumber[key] = 0
+class Animal {
+  constructor(animalTypes) {
+    this.types = animalTypes;
+    this.typesNumber = {};
+    animalTypes.forEach(function (type) {
+      this.typesNumber[type] = 0;
+    }.bind(this));
   }
 
-  var types = '';
-  for (var i = 0; i < numberOfAnimals; i++) {
-    var index = Math.floor(Math.random() * this.animalsTypes.length);
-    types += this.animalsTypes[index] + '\n';
+  getPopularType() {
+    let types = this.randomTypes.split('\n');
+    types.pop();
+    types.forEach(function (type) {
+      this.typesNumber[type]++
+    }.bind(this));
+
+    let popularType = [];
+    let maxValue = Math.max(...Object.values(this.typesNumber));
+    console.log(maxValue);
+
+    for (let key in this.typesNumber) {
+      if (this.typesNumber[key] === maxValue) {
+        popularType.push(key);
+      }
+    }
+
+    return popularType;
   }
 
-  if (this.getPopularType(types).length > 1) {
-    return this.generateAnimalsTypes(numberOfAnimals);
+  generateAnimals(numberOfAnimals) {
+
+    for (let key in this.typesNumber) {
+      this.typesNumber[key] = 0
+    }
+
+    let animals = '';
+    for (let i = 0; i < numberOfAnimals; i++) {
+      let index = Math.floor(Math.random() * this.types.length);
+      animals += this.types[index] + '\n';
+    }
+    this.randomTypes = animals;
+
+    if (this.getPopularType().length > 1) {
+      return this.generateAnimals(numberOfAnimals);
+    }
+
+    return animals;
   }
-  return types;
 }
 
-var lemurs = new Animal(['Карликовый', 'Руконожковый', 'Индриевый']);
+let lemurs = new Animal(['Карликовый', 'Руконожковый', 'Индриевый']);
+let errorMessage = new Error(document.querySelector('.error'));
 
-var input = document.querySelector('input');
-var resultCell = document.querySelector('.result');
-var dataCell = document.querySelector('.data');
-var generateDataBtn = document.querySelector('.generate-btn');
-var errorMessage = new Error(document.querySelector('.error'));
+const input = document.querySelector('input');
+const resultCell = document.querySelector('.result');
+const dataCell = document.querySelector('.data');
+const generateDataBtn = document.querySelector('.generate-btn');
+const getResultBtn = document.querySelector('.get-result-btn');
 
 generateDataBtn.addEventListener('click', function () {
   if (input.value <= 0 || !input.value) {
@@ -79,18 +87,16 @@ generateDataBtn.addEventListener('click', function () {
     if (resultCell.textContent) {
       resultCell.textContent = '';
     }
-    lemurs.allAnimals = lemurs.generateAnimalsTypes(parseInt(input.value, 10));
-    dataCell.textContent = input.value + '\n' + lemurs.allAnimals;
+
+    dataCell.textContent = input.value + '\n' + lemurs.generateAnimals(parseInt(input.value, 10));
   }
 });
-
-var getResultBtn = document.querySelector('.get-result-btn');
 
 getResultBtn.addEventListener('click', function() {
   if (!dataCell.textContent) {
     errorMessage.add('Необходимо сгенерировать данные');
   } else {
     errorMessage.remove();
-    resultCell.textContent = lemurs.getPopularType(lemurs.allAnimals);
+    resultCell.textContent = lemurs.getPopularType();
   }
 });
